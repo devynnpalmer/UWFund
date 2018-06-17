43488df05c5957be0f2b77eb08072534e4237fb5
devynnpalmer <palmerdevynn@gmail.com>
2018-06-17 16:49:44 -0400

    newRequest()

--- src/Fund.sol
+++ src/Fund.sol
@@ -7,9 +7,16 @@ contract Fund {
         address recipient;
         bool complete;
     }
+
+    Request public requests;
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
 
     function Fund(uint minimum) public {
         manager = msg.sender;
@@ -19,5 +26,17 @@ contract Fund {
     function contribute() public payable {
         require(msg.value > minimumContribution);
         approvers.push(msg.sender);
+
+    }
+
+    function createRequest(string description, uint value, address recipient) public restricted {
+       Request newRequest = Request({
+            description: description
+            value: value,
+            recipient: recipient,
+            complete: false
+        });
+
+        requests.push(newRequest);
     }
 }
