d555f0400b6a4928f9ae5ec3056afe39b5f68d6f
devynnpalmer <palmerdevynn@gmail.com>
2018-06-18 22:51:33 -0400

    typo

--- deploy.js
+++ deploy.js
@@ -1,4 +1,4 @@
-const HDWallerProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
