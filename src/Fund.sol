8a34f89c1e7eebed39a43c070001e7d9e79c983b
devynnpalmer <palmerdevynn@gmail.com>
2018-06-18 20:53:19 -0400

    added finalizeRequest() func

--- src/Fund.sol
+++ src/Fund.sol
@@ -14,6 +14,7 @@ contract Fund {
     address public manager;
     uint public minimumContribution;
     mapping(address => bool) public approvers;
+    uint approversCount;
 
     modifier restricted() {
         require(msg.sender == manager);
@@ -28,6 +29,7 @@ contract Fund {
     function contribute() public payable {
         require(msg.value > minimumContribution);
         approvers[msg.sender] = true;
+        approversCount++;
 
     }
 
@@ -50,5 +52,13 @@ contract Fund {
 
         request.approvals[msg.sender] = true;
         request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+        request.recipient.transfer(request.value);
+        request.complete = true;
     }
 }
